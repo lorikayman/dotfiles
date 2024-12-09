b840dd027fd75dd68b95b26f65270b99b6c420a3
Ironkayman <>
2024-12-09 15:39:28 +0400

    cz: Added draft pre/postinstall scripts

---
+++ run_once_after_apply-overrides.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo '''
+TODO:
+    - krita-wacom preasure curve value replacement in its config
+    - wacomlib and wcomtools
+'''
+
+echo this configuration is finished
---
+++ run_once_install-packages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt install \
+	flatpak \
+	curl \
+	zsh \
+	tilix
+
+# zinit install
+sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+flatpak install \
+	org.kde.krita \
+	org.keepassxc.KeePassXC \
+	net.drawpile.drawpile \
+	dev.lapce.lapce \
+	com.boxy_svg.BoxySVG \
+	org.inkscape.Inkscape \
+	io.dbeaver.DBeaverCommunity \
+	im.riot.Riot \
+	org.apache.directory.studio \
+	com.github.tchx84.Flatseal \
+	org.gimp.GIMP
+
