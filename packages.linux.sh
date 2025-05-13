424527f803ee05a3ecb7361d16a1ff3a9d68d33a
Lori Kayman <lori.j.kayman@gmail.com>
2025-05-13 22:48:25 +0400

    zed

--- packages.linux.sh
+++ packages.linux.sh
@@ -19,32 +19,17 @@ PKGS_MISC="
   p7zip-full \
   make \
   dmg2img \
-  tilix \
-  bleachbit \
   apt-rdepends"
 PKGS_FLATPAK="
   org.kde.krita \
-  io.github.OpenToonz \
   org.keepassxc.KeePassXC \
-  net.drawpile.drawpile \
-  dev.lapce.lapce \
-  com.boxy_svg.BoxySVG \
-  org.inkscape.Inkscape \
-  io.dbeaver.DBeaverCommunity \
-  im.riot.Riot \
-  org.apache.directory.studio \
   com.github.tchx84.Flatseal \
-  org.gimp.GIMP \
   org.kde.kwrite \
-  org.mozilla.firefox \
-  com.jgraph.drawio.desktop \
-  org.shotcut.Shotcut"
+  org.mozilla.firefox"
 PKGS_CARGO="
   ripgrep \
-  fd-find \
-  viu"
-PKGS_CARGO_LOCKED="
-  zellij"
+  zellij \
+  fd-find"
 
 sudo apt remove $PKGS_REMOVE
 rm -rf ~/.firefox
@@ -55,10 +40,8 @@ curl --fail --show-error --silent --location "https://raw.githubusercontent.com/
 curl --fail --show-error --silent --location "https://get.volta.sh" | zsh
 curl --fail --show-error --silent --location "https://astral.sh/uv/install.sh" | sh
 curl --fail --show-error --silent --location "https://sh.rustup.rs" | sh
-curl --fail --show-error --silent --location "https://ollama.com/install.sh" | sh
 
 flatpak install -y $PKGS_FLATPAK
-~/.cargo/bin/cargo install $PKGS_CARGO
 ~/.cargo/bin/cargo install --locked $PKGS_CARGO_LOCKED
 
 sudo apt install $PKGS_VT
