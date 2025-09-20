ec6fb9dedbeb5c4d3675fd4eff818ff2659c8598
Lori Kayman <lori.j.kayman@gmail.com>
2025-09-20 22:39:03 +0400

    packages.linux.sh

--- packages.linux.sh
+++ packages.linux.sh
@@ -1,13 +1,23 @@
 set -x
 
+CURL_ANY="curl --fail --show-error --silent --location"
+
+# packages which for any reason had to be removed,
+# i.e. have flatpak-distributed alternatives
 PKGS_REMOVE="
   firefox"
+
+# common across systems packages
 PKGS_TOOLS_BASE="
   flatpak
   zsh
   git
   curl
   wget"
+
+# virtualization-specific packages,
+# weather current environment is a virtual machine
+# or a future host of those
 PKGS_VT="
   qemu \
   qemu-system-arm \
@@ -21,28 +31,48 @@ PKGS_MISC="
   dmg2img \
   apt-rdepends"
 PKGS_FLATPAK="
-  org.kde.krita \
-  org.keepassxc.KeePassXC \
+  org.mozilla.firefox \
   com.github.tchx84.Flatseal \
+  org.keepassxc.KeePassXC \
   org.kde.kwrite \
-  org.mozilla.firefox"
-PKGS_CARGO="
+  org.kde.krita"
+PKGS_RUST_LOCKED="
   ripgrep \
   zellij \
   fd-find"
 
+LOCAL_BIN="$HOME/.local/bin"
+mkdir -p $LOCAL_BIN
+
 sudo apt remove $PKGS_REMOVE
 rm -rf ~/.firefox
+
 sudo apt install $PKGS_TOOLS_BASE
 
-curl --fail --show-error --silent --location https://taskfile.dev/install.sh | sh -s -- -d -b ~/.local/bin
-curl --fail --show-error --silent --location "https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh" | sh
-curl --fail --show-error --silent --location "https://get.volta.sh" | zsh
-curl --fail --show-error --silent --location "https://astral.sh/uv/install.sh" | sh
-curl --fail --show-error --silent --location "https://sh.rustup.rs" | sh
+$CURL_ANY \
+  https://taskfile.dev/install.sh |
+  sh -s -- -d -b $LOCAL_BIN
+$CURL_ANY \
+  https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh | sh
+$CURL_ANY \
+  https://get.volta.sh | sh
+$CURL_ANY \
+  https://astral.sh/uv/install.sh | sh
+$CURL_ANY \
+  https://sh.rustup.rs | sh
 
-flatpak install -y $PKGS_FLATPAK
-~/.cargo/bin/cargo install --locked $PKGS_CARGO_LOCKED
+chsh --shell /usr/bin/zsh
+# zinit packages are installed on the first run of zsh
+# we run it here
+zsh -f -c 'source "$HOME/.zshrc"'
 
+~/.cargo/bin/cargo install --locked $PKGS_RUST_LOCKED
 sudo apt install $PKGS_VT
 sudo apt install $PKGS_MISC
+
+$CURL_ANY \
+  https://github.com/sj14/epoch/releases/download/v0.2.2/epoch_0.2.2_linux_amd64 \
+  --output "$LOCAL_BIN/epoch"
+chmod +x "$LOCAL_BIN/epoch"
+
+flatpak install -y $PKGS_FLATPAK
